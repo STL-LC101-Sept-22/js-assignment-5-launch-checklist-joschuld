3814d7c8b19b439afbe1a44e3f5aa03f49da57d3
joschuld <91635911+joschuld@users.noreply.github.com>
2022-12-07 15:39:50 -0600

    autograder corrections 7! work plz work plz

--- scriptHelper.js
+++ scriptHelper.js
@@ -58,20 +58,28 @@ function formSubmission(document, list, pilot, copilot, fuelLevel, cargoLevel) {
     copilotStatus.innerHTML = `Co-pilot ${copilot} is ready for launch`;
     list.style.visibility = 'hidden';
 
-    //fuel wrong
+    //fuel wrong, else right
     if(fuelLevel < 10000) {
         list.style.visibility = 'visible'
         fuelStatus.innerHTML = 'Fuel level too low for launch'
         launchStatus.innerHTML = 'Shuttle Not Ready for Launch'
         launchStatus.style.color = 'rgb(199,37,78)'
+    } 
+    else { 
+        list.style.visibility = 'visible'
+        fuelStatus.innerHTML = 'Fuel level high enough for launch'
     }
 
-    //cargo wrong
+    //cargo wrong, else right
     if(cargoLevel > 10000){
         list.style.visibility = 'visible'
         cargoStatus.innerHTML = 'Cargo mass too heavy for launch'
         launchStatus.innerHTML = 'Shuttle Not Ready for Launch'
         launchStatus.style.color = 'rgb(199,37,78)'
+    } 
+    else {
+        list.style.visibility = 'visible'
+        cargoStatus.innerHTML = 'Cargo mass low enough for launch'
     }
 
 
@@ -97,7 +105,7 @@ function formSubmission(document, list, pilot, copilot, fuelLevel, cargoLevel) {
 
 
     // if both fuel/cargo right
-    else if(fuelLevel >= 10000 && cargoLevel <= 10000) {
+    if(fuelLevel >= 10000 && cargoLevel <= 10000) {
         list.style.visibility = 'visible'
         launchStatus.innerHTML = 'Shuttle is Ready for Launch'
         launchStatus.style.color = 'rgb(65, 159, 106)'
