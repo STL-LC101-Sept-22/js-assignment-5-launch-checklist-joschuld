bb8ebf8b403213b514bfc3fa30d78fa3515c58d1
joschuld <91635911+joschuld@users.noreply.github.com>
2022-12-07 12:32:54 -0600

    autograder corrections round 2

--- scriptHelper.js
+++ scriptHelper.js
@@ -61,14 +61,14 @@ function formSubmission(document, list, pilot, copilot, fuelLevel, cargoLevel) {
     if(fuelLevel < 10000) {
         list.style.visibility = 'visible'
         fuelStatus.innerHTML = 'Fuel level too low for launch'
-        launchStatus.innerHTML = 'Shuttle Not Ready for launch'
+        launchStatus.innerHTML = 'Shuttle Not Ready for Launch'
         launchStatus.style.color = 'rgb(199,37,78)'
     }
 
     else if(cargoLevel > 10000){
         list.style.visibility = 'visible'
         cargoStatus.innerHTML = 'Cargo mass too heavy for launch'
-        launchStatus.innerHTML = 'Shuttle Not Ready for launch'
+        launchStatus.innerHTML = 'Shuttle Not Ready for Launch'
         launchStatus.style.color = 'rgb(199,37,78)'
     }
 
